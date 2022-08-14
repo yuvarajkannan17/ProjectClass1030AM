9edfb9c81f09030f752de0240983d531d9fd252b
yuvarajkannan17 <yuvarajkannan17@gmail.com>
2022-08-14 11:15:30 +0530

    Added ClassA

---
+++ src/test/java/com/sample/ClassA.java
@@ -0,0 +1,20 @@
+package com.sample;
+
+public class ClassA {
+
+	private void method1() {
+		System.out.println("1");
+
+	}
+
+	private void method2() {
+		System.out.println("2");
+
+	}
+
+	private void method3() {
+		System.out.println("3");
+
+	}
+
+}
